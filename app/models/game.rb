cac7a5f8ece346ae12c46c3903a0ca9c428a5965
Shannon Rachal <srachal674@gmail.com>
2019-11-13 02:25:06 +0000

    Updated the db create games file.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,9 +1,6 @@
 class Game < ApplicationRecord
   def change
-    create_table :games do |t|
-      t.string :name
-      t.text :description
-      t.timestamps
+   
     end
   end
 end
--- db/migrate/20191113015630_create_games.rb
+++ db/migrate/20191113015630_create_games.rb
@@ -1,7 +1,8 @@
 class CreateGames < ActiveRecord::Migration[5.2]
   def change
     create_table :games do |t|
-
+      t.string :name
+      t.text :description
       t.timestamps
     end
   end
